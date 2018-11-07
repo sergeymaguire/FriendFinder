d62b8744e40523e2078c064b9051d5fe376ac140
sergeymaguire <sergeymaguire@gmail.com>
2018-11-07 11:24:04 -0800

    udating

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,30 @@
 let friendsData = require('../data/friends.js');
 
 function apiRoutes(app) {
+    app.get('/api/friendExist/name/:name', function (req, res) {
+        if (friendAlreadyExist(req.body.name))
+            res.json({
+                result: true
+            });
+        else
+            res.json({
+                result: false
+            });
+
+    });
+
     app.get('/api/friends', function (req, res) {
         res.json(friendsData);
     });
 
+ 
+
     app.post('/api/friends', function (req, res) {
-        if(!friendAlreadyExist(req.body.name)) {
+        if (!friendAlreadyExist(req.body.name)) {
             addNameToList(req.body);
+        } else{
+           res.json({exists: true});
+           return;
         }
         let friendIndex = getBestMatch(req.body.scores);
         //console.dir("req " + req);
@@ -16,6 +33,7 @@ function apiRoutes(app) {
     });
 
 }
+
 function addNameToList() {
 
 }
@@ -24,11 +42,11 @@ function getBestMatch(scores) {
     var total = 0,
         bestTotal = 99999,
         bestIndex = 0;
-    for(var i = 0; i < friendsData.length; i++) {
+    for (var i = 0; i < friendsData.length; i++) {
         //console.log("friendsData[i] " + friendsData[i].scores);
         //console.log("scores " + scores);
         total = matchFriends(friendsData[i], scores);
-        if(total < bestTotal) {
+        if (total < bestTotal) {
             //console.log("bestTotal " + bestTotal);
             bestTotal = total;
             bestIndex = i;
@@ -36,28 +54,29 @@ function getBestMatch(scores) {
     }
     return bestIndex;
 }
-function friendAlreadyExist (name) {
-    for(var i = 0; i < friendsData.length; i++) {
-        if(friendsData[i].name === name)
-        return true;
-    } 
+
+var friendAlreadyExist = function (name) {
+    name = name.toLowerCase();
+    for (var i = 0; i < friendsData.length; i++) {
+        if (friendsData[i].name.toLowerCase() === name) {
+            console.log("this already exists! " + name);
+
+            return true;
+        }
+    }
     return false;
-    alert("this already exists!")
+
 }
+
 function matchFriends(friend, scores) {
     var total = 0;
-    for(var i = 0; i < scores.length; i++) {
-      var score = parseInt(scores[i]);
+    for (var i = 0; i < scores.length; i++) {
+        var score = parseInt(scores[i]);
         total = total + Math.abs(score - friend.scores[i]);
     }
     //console.log("scores " + scores);
     return total;
 
 }
-
-
-
-
-
 
 module.exports = apiRoutes;
